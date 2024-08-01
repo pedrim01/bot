5e8db113b99832195484110ee902c22f85403c74
pedrim01 <peuu.aguiar@gmail.com>
2024-08-01 12:55:54 -0300

    '01-08-24-pt3'

--- encaminharTip.py
+++ encaminharTip.py
@@ -42,7 +42,7 @@ async def handler(event):
 # Inicie o cliente
 async def start_bot():
     try:
-        await client.start()
+        await client.start(bot_token=TOKEN)
         logger.info("Client is running. Listening for messages...")
         await client.run_until_disconnected()
 
